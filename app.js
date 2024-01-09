0fd33b5854ed25a8f65504b6c3a68616cb4ec5ca
Dania Garcia <daniagm75@gmail.com>
2024-01-09 10:42:44 -0600

    Mover snake con teclado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ snake.push({
     y: 1,
     xNext: 0,
     yNext: 0,
-    pinta: function(){
+    pinta: function () {
         ctx.font = '20px Serif';
         ctx.fillText('🐮', this.x * 20, this.y * 20);
     }
@@ -20,7 +20,7 @@ snake.push({
     y: 1,
     xNext: 2,
     yNext: 1,
-    pinta: function(){
+    pinta: function () {
         ctx.font = '20px Serif';
         ctx.fillText('🤍', this.x * 20, this.y * 20);
     }
@@ -30,7 +30,7 @@ snake.push({
     y: 1,
     xNext: 1,
     yNext: 1,
-    pinta: function(){
+    pinta: function () {
         ctx.font = '20px Serif';
         ctx.fillText('🤍', this.x * 20, this.y * 20);
     }
@@ -39,22 +39,22 @@ snake.push({
 const comida = {
     x: 0,
     y: 0,
-    aparece: function() {
+    aparece: function () {
         this.x = Math.floor(Math.random() * 30);
         this.y = Math.ceil(Math.random() * 20);
     },
-    pinta: function(){
+    pinta: function () {
         ctx.font = '25px Serif';
         ctx.fillText('🌽', this.x * 20, this.y * 20);
     }
 }
 
-function nextMove(){
+function nextMove() {
     snake.forEach((cuerpo, index) => {
-        if(index === 0){
+        if (index === 0) {
             cuerpo.x = posX;
             cuerpo.y = posY;
-        }else{
+        } else {
             cuerpo.x = cuerpo.xNext;
             cuerpo.y = cuerpo.yNext;
             cuerpo.xNext = snake[index - 1].x;
@@ -66,13 +66,31 @@ function nextMove(){
 let direccion = 1;
 
 setInterval(() => {
-    ctx.fillRect(0,0,600,400);
+    ctx.fillRect(0, 0, 600, 400);
     // comida.aparece();
     // comida.pinta();
     snake.forEach(cuerpo => cuerpo.pinta());
-    if(direccion === 1) posX++;
-    else if(direccion === 2) posY++;
-    else if(direccion === 3) posX--;
-    else posX--;
+    if (direccion === 1) posX++;
+    else if (direccion === 2) posY++;
+    else if (direccion === 3) posX--;
+    else posY--;
     nextMove();
 }, 500);
+
+document.querySelector('body')
+    .addEventListener('keydown', function(e){
+        switch(e.key){
+            case 'ArrowUp':
+                direccion = 4;
+                break;
+            case 'ArrowRight':
+                direccion = 1;
+                break;
+            case 'ArrowLeft':
+                direccion = 3;
+                break;
+            case 'ArrowDown':
+                direccion = 2;
+                break;
+        }
+    });
